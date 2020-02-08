937b8a96cfd1f3dafbde842b08f74df496c3f051
Nate Hatcher <nate.hatcher@gmail.com>
2020-02-07 18:30:23 -0800

    Start on neighbors function

--- index.js
+++ index.js
@@ -3,67 +3,86 @@ const check = require('check-types');
 const MINE = 'X';
 
 class Nsweeper {
-    constructor({ dim = 2, size = 10, density = 0.2 } = {}) {
-        this.maxBoardSize = 1000 * 1000 * 1000;
-        this.dim = dim;
-        this.size = size;
-        this.density = density;
-        this.moves = [];
-        this.validateSelf();
-        this.board = this.buildBoard(this.dim, this.size, this.density);
+  constructor({ dim = 2, size = 10, density = 0.2 } = {}) {
+    Nsweeper.validate({ dim, size, density, maxBoardSize: 1000 * 1000 * 1000 });
+
+    this.board = Nsweeper.buildBoard({ dim, size, density });
+
+    // For read use by front end
+    this.dim = dim;
+    this.size = size;
+    this.density = density;
+
+    // List of indexes selected by player in order of selection
+    this.moves = [];
+  }
+
+  static buildBoard({ dim, size, density }) {
+    function createMine() {
+      return Math.random() < density;
     }
 
-    buildBoard(dim, size, density) {
-        function createMine() {
-            return Math.random() < density;
+    // Size board
+    const board = new Array(Math.pow(size, dim));
+
+    // First pass, add mines
+    for (let i = 0; i < board.length; i++) {
+      if (createMine()) {
+        board[i] = MINE;
+      }
+    }
+
+    // Second pass, add numbers
+    for (let i = 0; i < board.length; i++) {
+      // Don't want to override mines with their neighbor mine count
+      if (board[i] === MINE) {
+        continue;
+      }
+      // Get neighbors based on size & dimensions
+      const neighbors = this.getNeighbors({ dim, size, index: i });
+
+      // Add up mines on neighbors
+      board[i] = neighbors.reduce((acc, cur) => {
+        if (cur === MINE) {
+          acc++;
         }
-
-        // Size board
-        const board = new Array(Math.pow(this.size, this.dim));
-
-        // First pass, add mines
-        for (let i = 0; i < board.length; i++) {
-            if (createMine()) {
-                board[i] = MINE;
-            }
-        }
-
-        // Second pass, add numbers
-        for (let i = 0; i < board.length; i++) {
-            if (board[i] === MINE) {
-                continue;
-            }
-            // Get neighbors based on size & dimensions
-            const neighbors = this.getNeighbors(i)
-                // Add up mines on neighbors
-            board[i] = neighbors.reduce((acc, cur) => {
-                if (cur === MINE) {
-                    acc++;
-                }
-                return acc;
-            }, 0)
-        }
-        return board;
+        return acc;
+      }, 0);
     }
+    return board;
+  }
 
-    getNeighbors(i) {
-        const n = [];
-        // TODO BUILD THIS FUNCTION
-        return n;
+  static getNeighbors({ dim, size, index }) {
+    // TODO INPROG, BROKEN
+    let n = [];
+    const withinDimIndex = index % size;
+    // Iterate through dimensions
+    for (let d = 0; d < dim; d++) {
+      const base = size * d + withinDimIndex;
+      n.push(base);
+      if (withinDimIndex !== 0) {
+        n.push(base - 1);
+      }
+      if (withinDimIndex !== size - 1) {
+        n.push(base + 1);
+      }
     }
+    // Remove the original index
+    n.splice(n.indexOf(index), 1);
+    return n;
+  }
 
-    validateSelf() {
-        check.assert.integer(this.dim);
-        check.assert.greaterOrEqual(this.dim, 1);
-        check.assert.integer(this.size);
-        check.assert.greaterOrEqual(this.size, 1);
-        check.assert.number(this.density);
-        check.assert.greaterOrEqual(this.density, 0);
-        check.assert.lessOrEqual(this.density, 1);
+  static validate({ dim, size, density, maxBoardSize }) {
+    check.assert.integer(dim);
+    check.assert.greaterOrEqual(dim, 1);
+    check.assert.integer(size);
+    check.assert.greaterOrEqual(size, 1);
+    check.assert.number(density);
+    check.assert.greaterOrEqual(density, 0);
+    check.assert.lessOrEqual(density, 1);
 
-        check.assert.lessOrEqual(Math.pow(this.size, this.dim), this.maxBoardSize);
-    }
+    check.assert.lessOrEqual(Math.pow(size, dim), maxBoardSize);
+  }
 }
-
 
 module.exports = Nsweeper;
--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,125 @@
 const assert = require('chai').assert;
 const Nsweeper = require('../index');
 
-
 describe('nsweeper', function() {
-    describe('index', function() {
-        it('should create a game', function() {
-            const game = new Nsweeper();
-            assert.exists(game);
-        });
-        it('should create a game that handles args', function() {
-            const [dim, size, density] = [1, 5, .1];
-            const game = new Nsweeper({ dim, size, density });
-            assert(game.dim === dim);
-            assert(game.size === size);
-            assert(game.density === density);
-        });
-        it('should throw if dim/size args out of bounds', function() {
-            let err;
-            const [dim, size, density] = [100000 - 0, 10000000, 0.1];
-            try {
-                const game = new Nsweeper({ dim, size, density });
-            } catch (e) {
-                err = e;
-            }
-            assert.exists(err);
-        });
-
+  describe('constructor', function() {
+    it('should create a game', function() {
+      const game = new Nsweeper();
+      assert.exists(game);
     });
+    it('should create a game that handles args', function() {
+      const [dim, size, density] = [1, 5, 0.1];
+      const game = new Nsweeper({ dim, size, density });
+      assert(game.dim === dim);
+      assert(game.size === size);
+      assert(game.density === density);
+    });
+  });
+
+  describe('getNeighbors', function() {
+    describe('1 dimension', function() {
+      it('should work', function() {
+        const [dim, size, index] = [1, 5, 3];
+        const neighbors = Nsweeper.getNeighbors({ dim, size, index });
+        assert(Array.isArray(neighbors));
+        assert(neighbors.length == 2);
+        assert.sameMembers(neighbors, [2, 4]);
+      });
+    });
+
+    describe('2 dimensions', function() {
+      it('top of grid', function() {
+        const [dim, size, index] = [2, 5, 3];
+        const neighbors = Nsweeper.getNeighbors({ dim, size, index });
+        assert(Array.isArray(neighbors));
+        assert(neighbors.length == 5);
+        assert.sameMembers(neighbors, [2, 4, 7, 8, 9]);
+      });
+
+      it('middle of grid', function() {
+        const [dim, size, index] = [2, 5, 12];
+        const neighbors = Nsweeper.getNeighbors({ dim, size, index });
+        assert(Array.isArray(neighbors));
+        assert(neighbors.length == 8);
+        assert.sameMembers(neighbors, [6, 7, 8, 11, 13, 16, 17, 18]);
+      });
+
+      it('bottom of grid', function() {
+        const [dim, size, index] = [2, 5, 21];
+        const neighbors = Nsweeper.getNeighbors({ dim, size, index });
+        assert(Array.isArray(neighbors));
+        assert(neighbors.length == 5);
+        assert.sameMembers(neighbors, [20, 15, 16, 17, 22]);
+      });
+
+      it('left middle of grid', function() {
+        const [dim, size, index] = [2, 5, 10];
+        const neighbors = Nsweeper.getNeighbors({ dim, size, index });
+        assert(Array.isArray(neighbors));
+        assert(neighbors.length == 5);
+        assert.sameMembers(neighbors, [5, 6, 11, 15, 16]);
+      });
+
+      it('right bottom corner of grid', function() {
+        const [dim, size, index] = [2, 5, 24];
+        const neighbors = Nsweeper.getNeighbors({ dim, size, index });
+        assert(Array.isArray(neighbors));
+        assert(neighbors.length == 3);
+        assert.sameMembers(neighbors, [18, 19, 23]);
+      });
+    });
+  });
+
+  describe('validation', function() {
+    it('should throw if dim/size args too large', function() {
+      let err;
+      const [dim, size, density] = [10000000, 10000000, 0.1];
+      try {
+        const game = new Nsweeper({ dim, size, density });
+      } catch (e) {
+        err = e;
+      }
+      assert.exists(err);
+    });
+    it('should throw if dim arg is negative', function() {
+      let err;
+      const [dim, size, density] = [-1, 10000000, 0.1];
+      try {
+        const game = new Nsweeper({ dim, size, density });
+      } catch (e) {
+        err = e;
+      }
+      assert.exists(err);
+    });
+    it('should throw if size arg is negative', function() {
+      let err;
+      const [dim, size, density] = [10000000, -1, 0.1];
+      try {
+        const game = new Nsweeper({ dim, size, density });
+      } catch (e) {
+        err = e;
+      }
+      assert.exists(err);
+    });
+    it('should throw if density arg too high', function() {
+      let err;
+      const [dim, size, density] = [10000000, 10000000, 1.1];
+      try {
+        const game = new Nsweeper({ dim, size, density });
+      } catch (e) {
+        err = e;
+      }
+      assert.exists(err);
+    });
+    it('should throw if density arg too low', function() {
+      let err;
+      const [dim, size, density] = [10000000, 10000000, -0.1];
+      try {
+        const game = new Nsweeper({ dim, size, density });
+      } catch (e) {
+        err = e;
+      }
+      assert.exists(err);
+    });
+  });
 });
