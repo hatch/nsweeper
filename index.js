c7995d77e8f768cef9e28c34d1dad266711b4b16
Nate Hatcher <nate.hatcher@gmail.com>
2020-02-07 16:53:36 -0800

    First pass at board building and param validation

--- index.js
+++ index.js
@@ -1,16 +1,67 @@
 'use strict';
+const check = require('check-types');
+const MINE = 'X';
 
 class Nsweeper {
-    constructor(dim, size, density) {
-        this.dim = dim || 2;
-        this.size = size || 10;
+    constructor({ dim = 2, size = 10, density = 0.2 } = {}) {
+        this.maxBoardSize = 1000 * 1000 * 1000;
+        this.dim = dim;
+        this.size = size;
+        this.density = density;
         this.moves = [];
-        this.density = density || .2;
-        this.board = this.buildBoard();
+        this.validateSelf();
+        this.board = this.buildBoard(this.dim, this.size, this.density);
     }
 
-    buildBoard() {
+    buildBoard(dim, size, density) {
+        function createMine() {
+            return Math.random() < density;
+        }
 
+        // Size board
+        const board = new Array(Math.pow(this.size, this.dim));
+
+        // First pass, add mines
+        for (let i = 0; i < board.length; i++) {
+            if (createMine()) {
+                board[i] = MINE;
+            }
+        }
+
+        // Second pass, add numbers
+        for (let i = 0; i < board.length; i++) {
+            if (board[i] === MINE) {
+                continue;
+            }
+            // Get neighbors based on size & dimensions
+            const neighbors = this.getNeighbors(i)
+                // Add up mines on neighbors
+            board[i] = neighbors.reduce((acc, cur) => {
+                if (cur === MINE) {
+                    acc++;
+                }
+                return acc;
+            }, 0)
+        }
+        return board;
+    }
+
+    getNeighbors(i) {
+        const n = [];
+        // TODO BUILD THIS FUNCTION
+        return n;
+    }
+
+    validateSelf() {
+        check.assert.integer(this.dim);
+        check.assert.greaterOrEqual(this.dim, 1);
+        check.assert.integer(this.size);
+        check.assert.greaterOrEqual(this.size, 1);
+        check.assert.number(this.density);
+        check.assert.greaterOrEqual(this.density, 0);
+        check.assert.lessOrEqual(this.density, 1);
+
+        check.assert.lessOrEqual(Math.pow(this.size, this.dim), this.maxBoardSize);
     }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -8,5 +8,23 @@ describe('nsweeper', function() {
             const game = new Nsweeper();
             assert.exists(game);
         });
+        it('should create a game that handles args', function() {
+            const [dim, size, density] = [1, 5, .1];
+            const game = new Nsweeper({ dim, size, density });
+            assert(game.dim === dim);
+            assert(game.size === size);
+            assert(game.density === density);
+        });
+        it('should throw if dim/size args out of bounds', function() {
+            let err;
+            const [dim, size, density] = [100000 - 0, 10000000, 0.1];
+            try {
+                const game = new Nsweeper({ dim, size, density });
+            } catch (e) {
+                err = e;
+            }
+            assert.exists(err);
+        });
+
     });
 });
